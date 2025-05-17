31c583b1e0dab49c11820336be5df6d1297597ce
Ulises <ulises@prosal.io>
2025-05-17 01:34:24 -0300

    noComments

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ from flask import Flask, render_template, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 
-# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET_KEY')  # Cargar la clave secreta desde el archivo .env
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 oauth = OAuth(app)
 
-# Configuración de Google OAuth
 google = oauth.register(
     name='google',
-    client_id=os.getenv('GOOGLE_CLIENT_ID'),  # Cargar el Client ID desde el archivo .env
-    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),  # Cargar el Client Secret desde el archivo .env
+    client_id=os.getenv('GOOGLE_CLIENT_ID'),
+    client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     client_kwargs={'scope': 'openid email profile'},
@@ -22,7 +20,7 @@ google = oauth.register(
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Renderiza index.html
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
